5c383657476bd82b20d54dd763b4edcaa383735d
DimaMukhin <dima9348@gmail.com>
2019-01-09 20:45:17 -0600

    implemented keyboard and mouse input

---
+++ OpenGLCourseApp/Camera.cpp
@@ -0,0 +1,13 @@
+#include "Camera.h"
+
+
+
+Camera::Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch)
+{
+	
+}
+
+
+Camera::~Camera()
+{
+}
--- OpenGLCourseApp/Window.cpp
+++ OpenGLCourseApp/Window.cpp
@@ -4,12 +4,22 @@ Window::Window()
 {
 	width = 800;
 	height = 600;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight)
 {
 	width = windowWidth;
 	height = windowHeight;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
 }
 
 int Window::Initialize()
@@ -43,6 +53,12 @@ int Window::Initialize()
 	// set context for GLEW to use
 	glfwMakeContextCurrent(mainWindow);
 
+	// Handle key + mouse input
+	createCallbacks();
+
+	// lock mouse to center of window
+	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	// Allow modern extension features
 	glewExperimental = GL_TRUE;
 
@@ -59,6 +75,72 @@ int Window::Initialize()
 
 	// setup viewport size (set drawable part of window)
 	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// this is necessary for using the static function "handleKeys"
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Window::createCallbacks()
+{
+	// when key is pressed on main window, call the function "handleKeys"
+	glfwSetKeyCallback(mainWindow, handleKeys);
+
+	// when mouse is moved on main window, call the function "handleMouse"
+	glfwSetCursorPosCallback(mainWindow, handleMouse);
+}
+
+GLfloat Window::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+void Window::handleKeys(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window)); // getting the instance of "Window.h"
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+		}
+	}
+}
+
+void Window::handleMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window)); // getting the instance of "Window.h"
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos; // to avoid inverted y axis
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
 }
 
 Window::~Window()
